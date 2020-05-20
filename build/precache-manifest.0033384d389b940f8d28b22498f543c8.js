30aba88af02f5419309cca3f44e441e984782efe
Pawel Olejko <pawel.olejko@avanade.com>
2020-05-20 12:49:17 +0200

    build

---
+++ build/precache-manifest.0033384d389b940f8d28b22498f543c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b36eb41b68d252c7dcae871fdb604df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49baf993ce660dc7116f",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "5140997f06a0bf4d249c",
+    "url": "/static/css/main.62965786.chunk.css"
+  },
+  {
+    "revision": "49baf993ce660dc7116f",
+    "url": "/static/js/2.41c7b591.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/static/js/2.41c7b591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5140997f06a0bf4d249c",
+    "url": "/static/js/main.9939f804.chunk.js"
+  },
+  {
+    "revision": "6cf236c4b3222b290f12",
+    "url": "/static/js/runtime-main.22592d92.js"
+  }
+]);
